271f9b8fde41f88e74ea034c8e17a1d43dff04fa
srinu8279 <91134725+srinu8279@users.noreply.github.com>
2022-08-23 10:42:12 +0530

    Deleted

--- Srinu_Java_Stuff/SpringBppt & Microsewrvices Docs/Code/BootDataProj-17-WorkingwithAssociationMapping/src/main/java/com/example/demo/repository/IPersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Person;
-
-public interface IPersonRepository extends JpaRepository<Person, Integer> {
-
-}
--- Srinu_Java_Stuff/SpringBppt & Microsewrvices Docs/Code/BootDataProj-17-WorkingwithAssociationMapping/src/main/java/com/example/demo/repository/IPhoneNumberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.PhoneNumber;
-
-public interface IPhoneNumberRepository extends JpaRepository<PhoneNumber, Integer> {
-
-}
