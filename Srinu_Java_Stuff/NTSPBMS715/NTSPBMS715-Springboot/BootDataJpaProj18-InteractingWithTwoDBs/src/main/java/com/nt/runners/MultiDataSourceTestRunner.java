de188c29b679a427a8a9d4f28b496929215b9a6c
malleswar-reddy <56329087+malleswar-reddy@users.noreply.github.com>
2022-09-17 22:43:34 +0530

    JPA  connection fixed

--- Srinu_Java_Stuff/NTSPBMS715/NTSPBMS715-Springboot/BootDataJpaProj18-InteractingWithTwoDBs/src/main/java/com/nt/runners/MultiDataSourceTestRunner.java
+++ Srinu_Java_Stuff/NTSPBMS715/NTSPBMS715-Springboot/BootDataJpaProj18-InteractingWithTwoDBs/src/main/java/com/nt/runners/MultiDataSourceTestRunner.java
@@ -1,16 +1,15 @@
 package com.nt.runners;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
 import com.nt.model.offers.Offers;
 import com.nt.model.product.Product;
 import com.nt.repo.offers.IOffersRepo;
 import com.nt.repo.product.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Component
 public class MultiDataSourceTestRunner implements CommandLineRunner {
@@ -21,15 +20,15 @@ public class MultiDataSourceTestRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		/*//prepare  model class objs
+		//prepare  model class objs
 		Product prod=new Product("table",900000.0,10.0);
 		Offers offers1=new Offers("monsoon offer", 10.0, LocalDate.of(2022,11,20));
 		Offers offers2=new Offers("winter offer", 17.0, LocalDate.of(2022,10,20));
 		//save Product info  in oracle db table
 		 System.out.println("Product is saved to Oracle with id value::"+prodRepo.save(prod).getPid());
 		 //save offers in mysql  db table
-		 int  count=offersRepo.saveAll(List.of(offers1,offers2)).size();
-		 System.out.println(count+" no.of offerrs are saved");*/
+		 int  count=offersRepo.saveAll(Arrays.asList(offers1,offers2)).size();
+		 System.out.println(count+" no.of offerrs are saved");
 		
 		System.out.println("The Products Info");
 		prodRepo.findAll().forEach(System.out::println);
